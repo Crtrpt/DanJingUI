206cc9e967fdad684fd44d55b01ac847e6fd43df
jingdor <945627077@qq.com>
2020-08-13 17:21:22 +0800

    '更新首页显示'

--- src/uikit/layout/layout1/TopMenu.vue
+++ src/uikit/layout/layout1/TopMenu.vue
@@ -2,20 +2,20 @@
     <div class="topNav">
       <div class="handler"><i class="fas fa-ellipsis-v"></i></div>
       <slot name="left">
-        <ul class="left">
-            <li class="item" v-for="m in menus" :key="m.id">
-              <div class="ui__icon"> {{m.icon}}</div>
-              <div class="ui__name"> {{m.name}}</div>
-            </li>
-        </ul>
+        <div class="left">
+            <a :href="m.href" class="item" v-for="m in menus" :key="m.id">
+              <img class="icon" :src="m.icon"  height="16px">
+              <div class="name"> {{m.name}}</div>
+            </a>
+        </div>
       </slot>
       <slot name="right">
-        <ul class="right">
-            <li class="item" v-for="m in menus" :key="m.id">
-              <div class="ui__icon"> {{m.icon}}</div>
-              <div class="ui__name"> {{m.name}}</div>
-            </li>
-        </ul>
+        <div class="right">
+            <a :href="m.href" class="item" v-for="m in rightMenu" :key="m.id">
+              <img class="icon" :src="m.icon"  height="16px">
+              <div class="name">{{m.name}}</div>
+            </a>
+        </div>
       </slot>
     </div>
 </template>
@@ -27,28 +27,46 @@ export default {
       return {
         menus:[
           {
-            id:1,
-            icon:"",
-            name:"Vue",
-            children:[],
-          },
-          {
+            href:"#/laravel",
             id:2,
-            icon:"",
+            icon:"/material/laravel_32x32.png",
             name:"Laravel",
             children:[],
           },
           {
+            href:"#/springboot",
             id:3,
-            icon:"",
+            icon:"/material/springboot_32x32.png",
             name:"Springboot",
             children:[],
           },
           {
+            href:"#/thinkphp",
             id:4,
-            icon:"",
+            icon:"/material/thinkphp_32x32.gif",
             name:"ThinkPHP",
             children:[],
+          },
+          {
+            href:"#/beego",
+            id:2,
+            icon:"/material/beego_20x20.png",
+            name:"Geego",
+            children:[],
+          },
+        ],
+        rightMenu:[
+          {
+            href:"https://github.com/Crtrpt/DanJingUI.git",
+            id:1,
+            icon:"/material/github_32x32.png",
+            name:"Github",
+          },
+          {
+            href:"https://gitee.com/945627077/DanJingUI.git",
+            id:2,
+            icon:"/material/gitee_32x32.ico",
+            name:"Gitee",
           },
         ]
       }
@@ -78,17 +96,27 @@ export default {
     line-height: 60px;
     font-size: 20px;
   }
-  .left li, .right li {
+  .left .item, .right .item {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    justify-items: center;
+    align-content: center;
     line-height: 60px;
-  }
+    align-items: center;
 
-   .right {
+    font-size: 14px;
+  }
+  .icon {
+    margin-right: 10px;
+  }
+  .right {
     flex-grow: 1;
     display: flex;
     flex-direction: row;
     justify-content: flex-end;
-   }
-   .item {
-     padding: 0px 10px;
-   }
+  }
+  .item {
+    padding: 0px 10px;
+  }
 </style>
